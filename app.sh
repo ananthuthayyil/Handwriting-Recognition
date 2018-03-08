8c70210406b773e9c144680c34ddb73b79a9d731
Ananthu <33564167+ananthuthayyil@users.noreply.github.com>
2018-03-08 20:38:33 +0530

    Create app.sh

---
+++ app.sh
@@ -0,0 +1,30 @@
+PORT=${PORT:-8080}
+
+ARGS=""
+
+ARGS="$ARGS --log-to-terminal"
+ARGS="$ARGS --port $PORT"
+ARGS="$ARGS --document-root htdocs"
+ARGS="$ARGS --url-alias /media media"
+
+if [ x"$MOD_WSGI_RELOAD_ON_CHANGES" != x"" ]; then
+    ARGS="$ARGS --reload-on-changes"
+fi
+
+if [ x"$MOD_WSGI_DEBUG_MODE" != x"" ]; then
+    ARGS="$ARGS --debug-mode"
+fi
+
+if [ x"$MOD_WSGI_ENABLE_DEBUGGER" != x"" ]; then
+    ARGS="$ARGS --enable-debugger"
+fi
+
+if [ x"$MOD_WSGI_ENABLE_WEBDAV" != x"" ]; then
+    ARGS="$ARGS --include configs/webdav.conf"
+fi
+
+if [ x"$BLOG_ENABLE_CRON_JOBS" != x"" ]; then
+    ARGS="$ARGS --service-script cronjobs cronjobs.py"
+fi
+
+exec python manage.py runmodwsgi $ARGS
